ab407e4e5104c57b8d5e4f0626838f20b2fbfe2f
fResult <styxmaz@gmail.com>
2025-04-03 00:13:07 +0700

    Re-format files

--- solutions/002_adapted_response_pattern/src/main/java/com/akka/training/Barista.java
+++ solutions/002_adapted_response_pattern/src/main/java/com/akka/training/Barista.java
@@ -12,76 +12,84 @@ import java.util.Set;
 
 public class Barista extends AbstractBehavior<Barista.BaristaCommand> {
 
-    // Orders <Whom, Coffee>
-    private final Map<String, Coffee> orders = new HashMap<>();
-    // reference to the coffee-machine child actor, allowing to send messages to coffee machine
-    private final ActorRef<CoffeeMachine.CoffeeMachineCommand> coffeeMachine;
-    private final ActorRef<CoffeeMachine.CoffeeIsReady> coffeeMachineMessageAdapter;
+  // Orders <Whom, Coffee>
+  private final Map<String, Coffee> orders = new HashMap<>();
+  // reference to the coffee-machine child actor, allowing to send messages to coffee machine
+  private final ActorRef<CoffeeMachine.CoffeeMachineCommand> coffeeMachine;
+  private final ActorRef<CoffeeMachine.CoffeeIsReady> coffeeMachineMessageAdapter;
 
-    private Barista(ActorContext<BaristaCommand> context) {
-        super(context);
-        // We spawn the CoffeeMachine as child actor in the private constructor where we have access to both
-        // context and class fields.
-        // It returns an ActorRef that we need to keep in the state,
-        // we will need it to interact with the CoffeeMachine.
-        coffeeMachine = context.spawn(CoffeeMachine.create(), "coffee-machine");
+  private Barista(ActorContext<BaristaCommand> context) {
+    super(context);
+    // We spawn the CoffeeMachine as child actor in the private constructor where we have access to
+    // both
+    // context and class fields.
+    // It returns an ActorRef that we need to keep in the state,
+    // we will need it to interact with the CoffeeMachine.
+    coffeeMachine = context.spawn(CoffeeMachine.create(), "coffee-machine");
 
-        coffeeMachineMessageAdapter = context.messageAdapter(
-            CoffeeMachine.CoffeeIsReady.class, WrappedCoffeeMachineCoffeeIsReady::new
-        );
+    coffeeMachineMessageAdapter =
+        context.messageAdapter(
+            CoffeeMachine.CoffeeIsReady.class, WrappedCoffeeMachineCoffeeIsReady::new);
+  }
+
+  public static Behavior<BaristaCommand> create() {
+    return Behaviors.setup(Barista::new);
+  }
+
+  // Format the orders into expected format [whom1->coffee1,whom2->coffee2]
+  static String printOrders(Set<Map.Entry<String, Coffee>> orders) {
+    return orders.stream()
+        .map(kv -> String.format("%s->%s", kv.getKey(), kv.getValue()))
+        .reduce((acc, s) -> acc + "," + s)
+        .map(s -> "[" + s + "]")
+        .orElse("[" + "]");
+  }
+
+  @Override
+  public Receive<BaristaCommand> createReceive() {
+    return newReceiveBuilder()
+        .onMessage(OrderCoffee.class, this::onOrderCoffee)
+        .onMessage(WrappedCoffeeMachineCoffeeIsReady.class, this::onWrappedCoffeeMachineCoffeeReady)
+        .build();
+  }
+
+  private Behavior<BaristaCommand> onOrderCoffee(OrderCoffee command) {
+    orders.put(command.whom, command.coffee);
+    getContext().getLog().info("Orders:{}", printOrders(orders.entrySet()));
+
+    coffeeMachine.tell(new CoffeeMachine.BrewCoffee(command.coffee, coffeeMachineMessageAdapter));
+
+    return this;
+  }
+
+  private Behavior<BaristaCommand> onWrappedCoffeeMachineCoffeeReady(
+      WrappedCoffeeMachineCoffeeIsReady wrappedCoffeeReady) {
+    CoffeeMachine.CoffeeIsReady coffeeReady = wrappedCoffeeReady.coffeeReady;
+
+    getContext().getLog().info("Barista: Picking up {}", coffeeReady.coffee);
+
+    coffeeMachine.tell(new CoffeeMachine.PickupCoffee());
+
+    return this;
+  }
+
+  interface BaristaCommand {}
+
+  public static final class OrderCoffee implements BaristaCommand {
+    public final String whom;
+    public final Coffee coffee;
+
+    public OrderCoffee(String whom, Coffee coffee) {
+      this.whom = whom;
+      this.coffee = coffee;
     }
+  }
 
-    public static Behavior<BaristaCommand> create() {
-        return Behaviors.setup(Barista::new);
+  public static final class WrappedCoffeeMachineCoffeeIsReady implements BaristaCommand {
+    public final CoffeeMachine.CoffeeIsReady coffeeReady;
+
+    public WrappedCoffeeMachineCoffeeIsReady(CoffeeMachine.CoffeeIsReady coffeeReady) {
+      this.coffeeReady = coffeeReady;
     }
-
-    // Format the orders into expected format [whom1->coffee1,whom2->coffee2]
-    static String printOrders(Set<Map.Entry<String,Coffee>> orders) {
-        return orders.stream()
-            .map(kv -> String.format("%s->%s", kv.getKey(), kv.getValue()))
-            .reduce((acc, s) -> acc + "," + s)
-            .map(s -> "[" + s + "]").orElse("[" + "]");
-    }
-
-    @Override
-    public Receive<BaristaCommand> createReceive() {
-        return newReceiveBuilder()
-            .onMessage(OrderCoffee.class, this::onOrderCoffee)
-            .onMessage(WrappedCoffeeMachineCoffeeIsReady.class, this::onWrappedCoffeeMachineCoffeeReady)
-            .build();
-    }
-
-    private Behavior<BaristaCommand> onOrderCoffee(OrderCoffee command) {
-        orders.put(command.whom, command.coffee);
-        getContext().getLog().info("Orders:{}", printOrders(orders.entrySet()));
-
-        coffeeMachine.tell(new CoffeeMachine.BrewCoffee(command.coffee, coffeeMachineMessageAdapter));
-
-        return this;
-    }
-
-    private Behavior<BaristaCommand> onWrappedCoffeeMachineCoffeeReady(WrappedCoffeeMachineCoffeeIsReady wrappedCoffeeReady) {
-        CoffeeMachine.CoffeeIsReady coffeeReady = wrappedCoffeeReady.coffeeReady;
-
-        getContext().getLog().info("Barista: Picking up {}", coffeeReady.coffee);
-
-        coffeeMachine.tell(new CoffeeMachine.PickupCoffee());
-
-        return this;
-    }
-
-    interface BaristaCommand { }
-
-    public static final class OrderCoffee implements BaristaCommand {
-        public final String whom;
-        public final Coffee coffee;
-        public OrderCoffee(String whom, Coffee coffee) { this.whom = whom; this.coffee = coffee; }
-    }
-
-    public static final class WrappedCoffeeMachineCoffeeIsReady implements BaristaCommand {
-        public final CoffeeMachine.CoffeeIsReady coffeeReady;
-        public WrappedCoffeeMachineCoffeeIsReady(CoffeeMachine.CoffeeIsReady coffeeReady) {
-            this.coffeeReady = coffeeReady;
-        }
-    }
+  }
 }
--- solutions/003_timers/src/main/java/com/akka/training/CoffeeMachine.java
+++ solutions/003_timers/src/main/java/com/akka/training/CoffeeMachine.java
@@ -8,72 +8,89 @@ import java.time.Duration;
 
 public class CoffeeMachine {
 
-    private static final int BREWING_DURATION_SECONDS = 10;
+  private static final int BREWING_DURATION_SECONDS = 10;
 
-    public static Behavior<CoffeeMachineCommand> create() {
-        return Behaviors.setup(context -> idle(context));
-    }
+  public static Behavior<CoffeeMachineCommand> create() {
+    return Behaviors.setup(context -> idle(context));
+  }
 
-    private static Behavior<CoffeeMachineCommand> idle(final ActorContext<CoffeeMachineCommand> context) {
-        context.getLog().info("CoffeeMachine: IDLE");
-        return Behaviors.receive(CoffeeMachineCommand.class)
-            .onMessage(BrewCoffee.class, command -> startBrewing(context, command))
-            // Can't pick up coffee until coffee is ready, stay in same behavior (equivalent to ignore the message)
-            .onMessage(PickupCoffee.class, command -> Behaviors.same())
-            .build();
-    }
+  private static Behavior<CoffeeMachineCommand> idle(
+      final ActorContext<CoffeeMachineCommand> context) {
+    context.getLog().info("CoffeeMachine: IDLE");
+    return Behaviors.receive(CoffeeMachineCommand.class)
+        .onMessage(BrewCoffee.class, command -> startBrewing(context, command))
+        // Can't pick up coffee until coffee is ready, stay in same behavior (equivalent to ignore
+        // the message)
+        .onMessage(PickupCoffee.class, command -> Behaviors.same())
+        .build();
+  }
 
-    private static Behavior<CoffeeMachineCommand> startBrewing(final ActorContext<CoffeeMachineCommand> context, BrewCoffee brewingCoffee) {
-        context.getLog().info("CoffeeMachine: Brewing 1 {}", brewingCoffee.coffee.toString());
-        return Behaviors.withTimers(timers -> {
-            timers.startSingleTimer(new CoffeeReadyTick(), Duration.ofSeconds(BREWING_DURATION_SECONDS));
-            return brewing(context, brewingCoffee);
+  private static Behavior<CoffeeMachineCommand> startBrewing(
+      final ActorContext<CoffeeMachineCommand> context, BrewCoffee brewingCoffee) {
+    context.getLog().info("CoffeeMachine: Brewing 1 {}", brewingCoffee.coffee.toString());
+    return Behaviors.withTimers(
+        timers -> {
+          timers.startSingleTimer(
+              new CoffeeReadyTick(), Duration.ofSeconds(BREWING_DURATION_SECONDS));
+          return brewing(context, brewingCoffee);
         });
-    }
+  }
 
-    private static Behavior<CoffeeMachineCommand> brewing(final ActorContext<CoffeeMachineCommand> context, BrewCoffee brewingCoffee) {
-        return Behaviors.receive(CoffeeMachineCommand.class)
-            .onMessage(CoffeeReadyTick.class, command -> {
-                brewingCoffee.replyTo.tell(new CoffeeIsReady(brewingCoffee.coffee));
-                return coffeeReady(context);
+  private static Behavior<CoffeeMachineCommand> brewing(
+      final ActorContext<CoffeeMachineCommand> context, BrewCoffee brewingCoffee) {
+    return Behaviors.receive(CoffeeMachineCommand.class)
+        .onMessage(
+            CoffeeReadyTick.class,
+            command -> {
+              brewingCoffee.replyTo.tell(new CoffeeIsReady(brewingCoffee.coffee));
+              return coffeeReady(context);
             })
-            .build();
+        .build();
+  }
+
+  // Protocol definition ->
+
+  private static Behavior<CoffeeMachineCommand> coffeeReady(
+      final ActorContext<CoffeeMachineCommand> context) {
+    context.getLog().info("CoffeeMachine: Coffee is ready");
+    return Behaviors.receive(CoffeeMachineCommand.class)
+        // Can't brew a new coffee until the ready one is picked-up, stay in same behavior
+        // (equivalent to ignore the message)
+        .onMessage(BrewCoffee.class, command -> Behaviors.same())
+        .onMessage(PickupCoffee.class, command -> idle(context))
+        .build();
+  }
+
+  // <- Protocol definition
+  public interface CoffeeMachineCommand {}
+
+  public static final class BrewCoffee implements CoffeeMachineCommand {
+    public final Coffee coffee;
+    public final ActorRef<CoffeeIsReady> replyTo;
+
+    public BrewCoffee(Coffee coffee, ActorRef<CoffeeIsReady> replyTo) {
+      this.coffee = coffee;
+      this.replyTo = replyTo;
     }
-    // Protocol definition ->
+  }
 
-    private static Behavior<CoffeeMachineCommand> coffeeReady(final ActorContext<CoffeeMachineCommand> context) {
-        context.getLog().info("CoffeeMachine: Coffee is ready");
-        return Behaviors.receive(CoffeeMachineCommand.class)
-            // Can't brew a new coffee until the ready one is picked-up, stay in same behavior (equivalent to ignore the message)
-            .onMessage(BrewCoffee.class, command -> Behaviors.same())
-            .onMessage(PickupCoffee.class, command -> idle(context))
-            .build();
+  public static final class CoffeeIsReady implements CoffeeMachineCommand {
+    public final Coffee coffee;
+
+    public CoffeeIsReady(Coffee coffee) {
+      this.coffee = coffee;
     }
+  }
 
-    // <- Protocol definition
-    public interface CoffeeMachineCommand { }
+  /*
+  Represents the Barista picking up the coffee and resetting the coffee machine, so that it's ready
+  for the next coffee
+   */
+  public static final class PickupCoffee implements CoffeeMachineCommand {}
 
-    public static final class BrewCoffee implements CoffeeMachineCommand {
-        public final Coffee coffee;
-        public final ActorRef<CoffeeIsReady> replyTo;
-        public BrewCoffee(
-                Coffee coffee,
-                ActorRef<CoffeeIsReady> replyTo
-        ) { this.coffee = coffee; this.replyTo = replyTo; }
-    }
-
-    public static final class CoffeeIsReady implements CoffeeMachineCommand {
-        public final Coffee coffee;
-        public CoffeeIsReady(Coffee coffee) { this.coffee = coffee; }
-    }
-
-    /*
-    Represents the Barista picking up the coffee and resetting the coffee machine, so that it's ready
-    for the next coffee
-     */
-    public static final class PickupCoffee implements CoffeeMachineCommand { }
-
-    // Note: CoffeeReadyTick is an internal message scheduled by the actor's timer that signal that brewing is completed.
-    // We make it private as it is for internal implementation and shouldn't be exposed as external protocol.
-    private static final class CoffeeReadyTick implements CoffeeMachineCommand {}
+  // Note: CoffeeReadyTick is an internal message scheduled by the actor's timer that signal that
+  // brewing is completed.
+  // We make it private as it is for internal implementation and shouldn't be exposed as external
+  // protocol.
+  private static final class CoffeeReadyTick implements CoffeeMachineCommand {}
 }
