150588312a75b0f01bec71752d475e296857c53a
fResult <styxmaz@gmail.com>
2025-04-06 03:58:54 +0700

    Refactor code.

--- exercises/java/002_adapted_response_pattern/src/main/java/com/akka/training/scratch/NewMain.java
+++ exercises/java/002_adapted_response_pattern/src/main/java/com/akka/training/scratch/NewMain.java
@@ -1,5 +1,7 @@
 package com.akka.training.scratch;
 
+import static com.akka.training.scratch.ClassUtils.simpleName;
+
 import akka.actor.typed.ActorRef;
 import akka.actor.typed.ActorSystem;
 import akka.actor.typed.Behavior;
@@ -7,6 +9,7 @@ import akka.actor.typed.javadsl.AbstractBehavior;
 import akka.actor.typed.javadsl.ActorContext;
 import akka.actor.typed.javadsl.Behaviors;
 import akka.actor.typed.javadsl.Receive;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -17,17 +20,31 @@ public class NewMain {
 
     baristaActor.tell(new BaristaCommand.OrderCoffee("Wick", new Coffee.CaffeJava()));
     baristaActor.tell(new BaristaCommand.OrderCoffee("Anderson", new Coffee.Akkacino()));
+
+    try {
+      System.out.println(">>> Please ENTER to exit <<<");
+      System.out.println(System.in.read());
+    } catch (IOException ex) {
+      throw new RuntimeException(ex);
+    } finally {
+      baristaActor.terminate();
+    }
   }
 }
 
 class BaristaAct extends AbstractBehavior<BaristaCommand> {
   private final Map<String, Coffee> orders = new HashMap<>();
   private final ActorRef<CoffeeMachineCommand> coffeeMachineActorRef;
+  private final ActorRef<CoffeeMachineCommand.CoffeeReady> coffeeMachineMessageAdapter;
 
   public BaristaAct(ActorContext<BaristaCommand> context) {
     super(context);
 
     coffeeMachineActorRef = context.spawn(CoffeeMachineAct.create(), "coffee-machine");
+    coffeeMachineMessageAdapter =
+        context.messageAdapter(
+            CoffeeMachineCommand.CoffeeReady.class,
+            BaristaCommand.WrappedCoffeeMachineCoffeeReady::new);
   }
 
   public static Behavior<BaristaCommand> create() {
@@ -38,12 +55,26 @@ class BaristaAct extends AbstractBehavior<BaristaCommand> {
   public Receive<BaristaCommand> createReceive() {
     return newReceiveBuilder()
         .onMessage(BaristaCommand.OrderCoffee.class, this::onOrderCoffee)
+        .onMessage(
+            BaristaCommand.WrappedCoffeeMachineCoffeeReady.class,
+            this::onWrappedCoffeeMachineCoffeeReady)
         .build();
+  }
+
+  private Behavior<BaristaCommand> onWrappedCoffeeMachineCoffeeReady(
+      BaristaCommand.WrappedCoffeeMachineCoffeeReady wrappedCommand) {
+
+    final var coffeeReadyCommand = wrappedCommand.command();
+    getContext().getLog().info("Barista: Picking up {}", simpleName(coffeeReadyCommand.coffee()));
+
+    coffeeMachineActorRef.tell(new CoffeeMachineCommand.PickupCoffee());
+
+    return this;
   }
 
   public static String printOrders(final Set<Map.Entry<String, Coffee>> orders) {
     return orders.stream()
-        .map(kv -> String.format("%s->%s", kv.getKey(), kv.getValue()))
+        .map(kv -> String.format("%s->%s", kv.getKey(), simpleName(kv.getValue())))
         .reduce((acc, s) -> acc + "," + s)
         .map(s -> "[" + s + "]")
         .orElse("[" + "]");
@@ -52,6 +83,9 @@ class BaristaAct extends AbstractBehavior<BaristaCommand> {
   private Behavior<BaristaCommand> onOrderCoffee(BaristaCommand.OrderCoffee command) {
     orders.put(command.whom(), command.coffee());
     getContext().getLog().info("Orders: {}", printOrders(orders.entrySet()));
+
+    coffeeMachineActorRef.tell(
+        new CoffeeMachineCommand.BrewCoffee(command.coffee(), coffeeMachineMessageAdapter));
 
     return Behaviors.same();
   }
@@ -66,16 +100,14 @@ class CoffeeMachineAct {
     context.getLog().info("CoffeeMachine: IDLE");
 
     return Behaviors.receive(CoffeeMachineCommand.class)
-        .onMessage(
-            CoffeeMachineCommand.BrewCoffee.class,
-            brewCoffeeCommand -> brewCoffee(brewCoffeeCommand, context))
-        .onMessage(CoffeeMachineCommand.PickupCoffee.class, pickupCommand -> Behaviors.same())
+        .onMessage(CoffeeMachineCommand.BrewCoffee.class, command -> brewCoffee(command, context))
+        .onMessage(CoffeeMachineCommand.PickupCoffee.class, command -> Behaviors.same())
         .build();
   }
 
   private static Behavior<CoffeeMachineCommand> brewCoffee(
       CoffeeMachineCommand.BrewCoffee command, ActorContext<CoffeeMachineCommand> context) {
-    context.getLog().info("CoffeeMachine: Brewing 1 {}", command.coffee());
+    context.getLog().info("CoffeeMachine: Brewing 1 {}", simpleName(command.coffee()));
 
     try {
       Thread.sleep(3000);
@@ -90,7 +122,7 @@ class CoffeeMachineAct {
 
   private static Behavior<CoffeeMachineCommand> coffeeReady(
       CoffeeMachineCommand.BrewCoffee command, ActorContext<CoffeeMachineCommand> context) {
-    context.getLog().info("CoffeeMachine: Coffee {} is ready", command.coffee());
+    context.getLog().info("CoffeeMachine: Coffee {} is ready", simpleName(command.coffee()));
 
     return Behaviors.receive(CoffeeMachineCommand.class)
         .onMessage(CoffeeMachineCommand.BrewCoffee.class, brewCoffeeCommand2 -> Behaviors.same())
@@ -99,10 +131,12 @@ class CoffeeMachineAct {
   }
 }
 
-sealed interface BaristaCommand permits BaristaCommand.OrderCoffee, BaristaCommand.CoffeeReady {
+sealed interface BaristaCommand
+    permits BaristaCommand.OrderCoffee, BaristaCommand.WrappedCoffeeMachineCoffeeReady {
   record OrderCoffee(String whom, Coffee coffee) implements BaristaCommand {}
 
-  record CoffeeReady(Coffee coffee) implements BaristaCommand {}
+  record WrappedCoffeeMachineCoffeeReady(CoffeeMachineCommand.CoffeeReady command)
+      implements BaristaCommand {}
 }
 
 sealed interface CoffeeMachineCommand
@@ -123,4 +157,10 @@ sealed interface Coffee permits Coffee.Akkacino, Coffee.MochaPlay, Coffee.CaffeJ
   record MochaPlay() implements Coffee {}
 
   record CaffeJava() implements Coffee {}
+}
+
+class ClassUtils {
+  public static <T> String simpleName(T instance) {
+    return instance.getClass().getSimpleName();
+  }
 }
