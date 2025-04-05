3c27b268af3c897889756e24d3e73173efea8cd2
fResult <styxmaz@gmail.com>
2025-04-05 17:41:50 +0700

    Log error in the `Main` class for exercises 1 and 2.

--- exercises/java/001_request_response_pattern/src/main/java/com/akka/training/Main.java
+++ exercises/java/001_request_response_pattern/src/main/java/com/akka/training/Main.java
@@ -2,8 +2,11 @@ package com.akka.training;
 
 import akka.actor.typed.ActorSystem;
 import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
+  private static final Logger log = LoggerFactory.getLogger(Main.class);
 
   public static void main(String[] args) {
     final ActorSystem<BaristaActor.BaristaCommand> actorSystem =
@@ -15,7 +18,8 @@ public class Main {
     try {
       System.out.println(">>> Press ENTER to exit <<<");
       System.in.read();
-    } catch (IOException ignored) {
+    } catch (IOException ex) {
+      log.error("Error: {}", ex.getMessage());
     } finally {
       actorSystem.terminate();
     }
