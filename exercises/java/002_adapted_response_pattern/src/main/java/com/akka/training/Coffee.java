10ca99c67f0c2e94bff22f917961fa74fbee4992
fResult <styxmaz@gmail.com>
2025-04-05 23:04:22 +0700

    Refactor from interface with final classes to sealed interface with records for exercise 2.

--- exercises/java/002_adapted_response_pattern/src/main/java/com/akka/training/Coffee.java
+++ exercises/java/002_adapted_response_pattern/src/main/java/com/akka/training/Coffee.java
@@ -1,41 +1,47 @@
 package com.akka.training;
 
-import java.io.Serializable;
+public sealed interface Coffee permits Coffee.Akkaccino, Coffee.CaffeJava, Coffee.MochaPlay {
+  record Akkaccino() implements Coffee {}
 
-public interface Coffee extends Serializable {
-  final class Akkaccino implements Coffee {
-    @Override
-    public String toString() {
-      return this.getClass().getSimpleName();
-    }
+  record CaffeJava() implements Coffee {}
 
-    @Override
-    public boolean equals(Object o) {
-      return o == this || o instanceof Akkaccino;
-    }
-  }
-
-  final class CaffeJava implements Coffee {
-    @Override
-    public String toString() {
-      return this.getClass().getSimpleName();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-      return o == this || o instanceof CaffeJava;
-    }
-  }
-
-  final class MochaPlay implements Coffee {
-    @Override
-    public String toString() {
-      return this.getClass().getSimpleName();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-      return o == this || o instanceof MochaPlay;
-    }
-  }
+  record MochaPlay() implements Coffee {}
 }
+
+// public interface Coffee extends Serializable {
+//  final class Akkaccino implements Coffee {
+//    @Override
+//    public String toString() {
+//      return this.getClass().getSimpleName();
+//    }
+//
+//    @Override
+//    public boolean equals(Object o) {
+//      return o == this || o instanceof Akkaccino;
+//    }
+//  }
+//
+//  final class CaffeJava implements Coffee {
+//    @Override
+//    public String toString() {
+//      return this.getClass().getSimpleName();
+//    }
+//
+//    @Override
+//    public boolean equals(Object o) {
+//      return o == this || o instanceof CaffeJava;
+//    }
+//  }
+//
+//  final class MochaPlay implements Coffee {
+//    @Override
+//    public String toString() {
+//      return this.getClass().getSimpleName();
+//    }
+//
+//    @Override
+//    public boolean equals(Object o) {
+//      return o == this || o instanceof MochaPlay;
+//    }
+//  }
+// }
