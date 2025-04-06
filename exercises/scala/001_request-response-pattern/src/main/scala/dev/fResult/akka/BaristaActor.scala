1f27c18ee1cdb2678e57a4906fa2f514da4ad610
fResult <styxmaz@gmail.com>
2025-04-06 19:19:14 +0700

    Create `orders` hash map to contain customer map to coffee.

--- exercises/scala/001_request-response-pattern/src/main/scala/dev/fResult/akka/BaristaActor.scala
+++ exercises/scala/001_request-response-pattern/src/main/scala/dev/fResult/akka/BaristaActor.scala
@@ -3,15 +3,21 @@ package dev.fResult.akka
 import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.{AbstractBehavior, ActorContext, Behaviors}
 
+import scala.collection.mutable
+
 object BaristaActor {
   def apply(): Behavior[OrderCoffee] = Behaviors.setup(BaristaBehavior(_))
 
+  final case class OrderCoffee(whom: String, coffee: Coffee)
+
   private final class BaristaBehavior(context: ActorContext[OrderCoffee]) extends AbstractBehavior[OrderCoffee](context):
+    private val orders = mutable.Map[String, Coffee]()
+
     override def onMessage(command: OrderCoffee): Behavior[OrderCoffee] = {
+      orders.put(command.whom, command.coffee)
+
       context.log.info(s"Greet, $command")
 
       Behaviors.same
     }
-
-  final case class OrderCoffee(whom: String, coffee: Coffee)
 }
