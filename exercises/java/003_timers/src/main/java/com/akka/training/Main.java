f5bc5ca5c260106342a2ad29439de5a796684bb8
fResult <styxmaz@gmail.com>
2025-04-15 15:22:24 +0700

    Fix compilation error in Main class for Java exercise 3.

--- exercises/java/003_timers/src/main/java/com/akka/training/Main.java
+++ exercises/java/003_timers/src/main/java/com/akka/training/Main.java
@@ -2,14 +2,15 @@ package com.akka.training;
 
 import akka.actor.typed.ActorSystem;
 import com.akka.training.barista.BaristaActor;
+import com.akka.training.barista.BaristaCommand;
 import java.io.IOException;
 
 public class Main {
   public static void main(String[] args) {
     final var baristaActor = ActorSystem.create(BaristaActor.create(), "barista");
 
-    baristaActor.tell(new BaristaActor.OrderCoffee("Bart", new Coffee.Akkaccino()));
-    baristaActor.tell(new BaristaActor.OrderCoffee("Lisa", new Coffee.MochaPlay()));
+    baristaActor.tell(new BaristaCommand.OrderCoffee("Bart", new Coffee.Akkaccino()));
+    baristaActor.tell(new BaristaCommand.OrderCoffee("Lisa", new Coffee.MochaPlay()));
 
     try {
       System.out.println(">>> Press ENTER to exit <<<");
