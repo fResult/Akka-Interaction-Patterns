80dcbde384750995572ab2a9a0f87cbbf40001f2
fResult <styxmaz@gmail.com>
2025-04-15 14:51:42 +0700

    Fix `CoffeeMachineActorTests` errors.

--- exercises/java/003_timers/src/test/java/com/akka/training/CoffeeMachineActorTests.java
+++ exercises/java/003_timers/src/test/java/com/akka/training/CoffeeMachineActorTests.java
@@ -5,28 +5,28 @@ import akka.actor.testkit.typed.javadsl.TestKitJunitResource;
 import akka.actor.testkit.typed.javadsl.TestProbe;
 import akka.actor.typed.ActorRef;
 import com.akka.training.coffeeMachine.CoffeeMachineActor;
+import com.akka.training.coffeeMachine.CoffeeMachineCommand;
 import java.time.Duration;
 import org.junit.ClassRule;
 import org.junit.Test;
 
 public class CoffeeMachineActorTests {
-
   @ClassRule public static final TestKitJunitResource testKit = new TestKitJunitResource();
   private final int BREWING_DURATION_Millis = 10000;
 
   // CoffeeMachine should transition from IDLE to Brewing on receiving BrewCoffee message
   @Test
   public void brewing() {
-    ActorRef<CoffeeMachineActor.CoffeeMachineCommand> coffeeMachine =
+    ActorRef<CoffeeMachineCommand> coffeeMachine =
         testKit.spawn(CoffeeMachineActor.create(), "coffee-machine-brewing");
-    TestProbe<CoffeeMachineActor.CoffeeIsReady> probe = testKit.createTestProbe();
+    TestProbe<CoffeeMachineCommand.CoffeeReady> probe = testKit.createTestProbe();
     var coffee = new Coffee.Akkaccino();
     LoggingTestKit.info("CoffeeMachine: Brewing 1 " + coffee)
         .withOccurrences(1)
         .expect(
             testKit.system(),
             () -> {
-              coffeeMachine.tell(new CoffeeMachineActor.BrewCoffee(coffee, probe.getRef()));
+              coffeeMachine.tell(new CoffeeMachineCommand.BrewCoffee(coffee, probe.getRef()));
               return null;
             });
 
@@ -35,16 +35,15 @@ public class CoffeeMachineActorTests {
 
   @Test
   public void shouldSendReadySignalToBarista() {
-    ActorRef<CoffeeMachineActor.CoffeeMachineCommand> coffeeMachine =
+    ActorRef<CoffeeMachineCommand> coffeeMachine =
         testKit.spawn(CoffeeMachineActor.create(), "coffee-machine-shouldSendReadySignalToBarista");
-    TestProbe<CoffeeMachineActor.CoffeeIsReady> probe = testKit.createTestProbe();
+    TestProbe<CoffeeMachineCommand.CoffeeReady> probe = testKit.createTestProbe();
 
-    coffeeMachine.tell(
-        new CoffeeMachineActor.BrewCoffee(new Coffee.Akkaccino(), probe.getRef()));
+    coffeeMachine.tell(new CoffeeMachineCommand.BrewCoffee(new Coffee.Akkaccino(), probe.getRef()));
 
     probe.expectNoMessage(Duration.ofMillis(BREWING_DURATION_Millis - 100));
     probe.expectMessageClass(
-        CoffeeMachineActor.CoffeeIsReady.class, Duration.ofMillis(BREWING_DURATION_Millis + 100));
+        CoffeeMachineCommand.CoffeeReady.class, Duration.ofMillis(BREWING_DURATION_Millis + 100));
 
     testKit.stop(coffeeMachine);
   }
@@ -54,44 +53,42 @@ public class CoffeeMachineActorTests {
   // The CoffeeMachine should be able a new brewing cycle.
   @Test
   public void restartCycle() {
-    ActorRef<CoffeeMachineActor.CoffeeMachineCommand> coffeeMachine =
+    ActorRef<CoffeeMachineCommand> coffeeMachine =
         testKit.spawn(CoffeeMachineActor.create(), "coffee-machine-restartCycle");
-    TestProbe<CoffeeMachineActor.CoffeeIsReady> probe = testKit.createTestProbe();
+    TestProbe<CoffeeMachineCommand.CoffeeReady> probe = testKit.createTestProbe();
 
-    coffeeMachine.tell(
-        new CoffeeMachineActor.BrewCoffee(new Coffee.Akkaccino(), probe.getRef()));
+    coffeeMachine.tell(new CoffeeMachineCommand.BrewCoffee(new Coffee.Akkaccino(), probe.getRef()));
     probe.expectNoMessage(Duration.ofMillis(BREWING_DURATION_Millis - 100));
     probe.expectMessageClass(
-        CoffeeMachineActor.CoffeeIsReady.class, Duration.ofMillis(BREWING_DURATION_Millis + 100));
-    coffeeMachine.tell(new CoffeeMachineActor.PickupCoffee());
+        CoffeeMachineCommand.CoffeeReady.class, Duration.ofMillis(BREWING_DURATION_Millis + 100));
+    coffeeMachine.tell(new CoffeeMachineCommand.PickupCoffee());
 
-    coffeeMachine.tell(
-        new CoffeeMachineActor.BrewCoffee(new Coffee.MochaPlay(), probe.getRef()));
+    coffeeMachine.tell(new CoffeeMachineCommand.BrewCoffee(new Coffee.MochaPlay(), probe.getRef()));
     probe.expectNoMessage(Duration.ofMillis(BREWING_DURATION_Millis - 100));
     probe.expectMessageClass(
-        CoffeeMachineActor.CoffeeIsReady.class, Duration.ofMillis(BREWING_DURATION_Millis + 100));
+        CoffeeMachineCommand.CoffeeReady.class, Duration.ofMillis(BREWING_DURATION_Millis + 100));
 
     testKit.stop(coffeeMachine);
   }
 
   // Once it started a brewing cycle, any other "BrewCoffee" command will be ignored.
-  // (First, Barista need to pickup the ready coffee to reset the machine to idle state)
+  // (First, Barista need to pick up the ready coffee to reset the machine to idle state)
   @Test
   public void shouldNotProcessCoffeesUntilReset() {
     var coffee1 = new Coffee.MochaPlay();
     var coffee2 = new Coffee.Akkaccino();
 
-    ActorRef<CoffeeMachineActor.CoffeeMachineCommand> coffeeMachine =
+    ActorRef<CoffeeMachineCommand> coffeeMachine =
         testKit.spawn(
             CoffeeMachineActor.create(), "coffee-machine-shouldNotProcessCoffeesUntilReset");
-    TestProbe<CoffeeMachineActor.CoffeeIsReady> probe = testKit.createTestProbe();
+    TestProbe<CoffeeMachineCommand.CoffeeReady> probe = testKit.createTestProbe();
 
-    coffeeMachine.tell(new CoffeeMachineActor.BrewCoffee(coffee1, probe.getRef()));
-    coffeeMachine.tell(new CoffeeMachineActor.BrewCoffee(coffee2, probe.getRef()));
+    coffeeMachine.tell(new CoffeeMachineCommand.BrewCoffee(coffee1, probe.getRef()));
+    coffeeMachine.tell(new CoffeeMachineCommand.BrewCoffee(coffee2, probe.getRef()));
 
     // We receive a CoffeeIsReady signal, expected
     probe.expectMessageClass(
-        CoffeeMachineActor.CoffeeIsReady.class, Duration.ofMillis(BREWING_DURATION_Millis + 1000));
+        CoffeeMachineCommand.CoffeeReady.class, Duration.ofMillis(BREWING_DURATION_Millis + 1000));
     // Then the CoffeeMachine stay in its state 'coffee ready' (the second brew coffee command was
     // ignored),
     // We are checking that by waiting and see that no new message is sent by the CoffeeMachine.
